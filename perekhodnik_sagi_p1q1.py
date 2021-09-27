b27f2d36da2044b9989296e2dbb9aad4c50a0ed1
Sagi Perekhodnik <sagipere2004@gmail.com>
2021-09-27 11:57:05 -0700

    Added the quiz file
    Created a template

---
+++ perekhodnik_sagi_p1q1.py
@@ -0,0 +1,17 @@
+# Example of <OUTPUT>
+print("I'm GreetingsBot.")
+print()
+
+# Say hi and then ask for the user's name
+print("Hello! What's your name?")
+# Get the user's name and STORE it somewhere
+# Example of <INPUT>
+# Example of <VARIABLE>
+username = input ()
+
+# Respond nicely using THE USER'S NAME
+print(f"Cool! It's nice to meet you {username}.")
+print()
+
+# Ask the user what their favourite book is
+print("let's have a quiz")
