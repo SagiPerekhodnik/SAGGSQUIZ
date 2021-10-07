fb4d19eeaf37601b2439f98e106ba63a5b8c55c2
Sagi Perekhodnik <sagipere2004@gmail.com>
2021-10-07 11:52:04 -0700

    Added the quiz file
    Created a template
    
    Sagittarius's Quiz
    
    1. Changed name
    2. created multiple choice questions

--- perekhodnik_sagi_p1q1.py
+++ perekhodnik_sagi_p1q1.py
@@ -16,18 +16,17 @@ print()
 print(f"Welcome {username}!")
 print()
 
-
+score = 0
+score == 0
 # Ask the user Question 1.
 answer1 = input("When is Sagi's birthday? \na. June 4 \nb. December 17 \nc. November 27 \nd. February 10 \nAnswer: ")
 if answer1 == "c" or answer1 == "November 27":
-    score == 0
     score += 1
     print("Correct")
     print("score: ", score)
     print("\n")
 else:
     score = 0
-    score += 1
     print("Incorrect! The answer was November 27.")
     print("Score: ", score)
     print("\n")
@@ -50,7 +49,7 @@ print()
 answer3 = input("What is Sagi's favourite sport? \na. swimming \nb. soccer \nc. hockey \nd. volleyball \nAnswer: ")
 if answer3 == "a" or answer3 == "swimming":
     score = 0
-    score += 1
+    score += 2
     print("Correct")
     print("Score: ", score)
     print("\n")
@@ -61,10 +60,10 @@ else:
 print()
 
 # Ask the user Question 4.
-answer4 = input("Calculate this math question to determine Sagi's favourite number: \na. 37 \nb. 46 \nc. 73 \nd. 11 \nAnswer: ")
+answer4 = input("What is Sagi's favourite number?: \na. 37 \nb. 46 \nc. 73 \nd. 11 \nAnswer: ")
 if answer4 == "a" or answer4 == "37":
     score = 0
-    score += 1
+    score += 3
     print("Correct")
     print("Score: ", score)
     print("\n")
@@ -73,6 +72,29 @@ else:
     print("score: ", score)
     print("\n")
 print()
+
+# Ask the user Question 5
+answer5 = input("The team Sagi is competing or coaching for our school this year for is: \na. Volleyball \nb. Soccer \nc. Swim team \nd. Swimming assistant Coach \nAnswer: ")
+if answer5 == "d" or answer5 == "Swimming assistant Coach":
+    score = 0
+    score += 4
+    print("Correct")
+    print("Score: ", score)
+    print("\n")
+else:
+    print("Incorrect! The answer was Swimming assistant Coach.")
+    print("score: ", score)
+    print("\n")
+print()
+
+print("Congratulations! You completed the quizzo about Sagi")
+print("Here are your results:")
+print(f"Total Score: {(score /5) * 100}%")
+
+
+
+
+
 
 
 
